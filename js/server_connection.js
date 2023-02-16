69a01ae283901160a610386c50aa56d7308ed247
A-me-lie <112852211+A-me-lie@users.noreply.github.com>
2023-02-16 13:21:58 +0100

    Html
    Starting to create the layout for html

---
+++ js/server_connection.js
@@ -0,0 +1,12 @@
+
+
+
+
+//här ska åtminstånde fetch funktionen finnas
+//prefix?action=check_credentials&user_name=X&password=Y
+
+//{
+// action: “register”,
+// user_name: string, username to register
+// password: string, password to register
+// }
