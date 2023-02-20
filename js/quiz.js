fab0829f9db1359a2bcda216aa3074415b0215b1
A-me-lie <112852211+A-me-lie@users.noreply.github.com>
2023-02-20 14:24:25 +0100

    creating quiz page

---
+++ js/quiz.js
@@ -0,0 +1,30 @@
+"use strict";
+
+let quiz_activator = false
+
+//när du får status 200 då ska quiz_activator vara lika med true
+//display_quiz tömmer register_login och 
+
+function display_quiz() {
+
+
+
+
+    content.innerHTML = ``;
+    content.innerHTML = `
+    // <div id="user_name_display>
+    // <p></p><button id="logout">logout</button>
+    //  <img src="" alt=""> create element
+    <button></button>
+    <button></button>
+    <button></button>
+    <button></button>
+
+</div>
+
+    
+    
+    
+    `;
+}
+
